79c2046f4dfe0844b5b4a939e9c055c2d65d6dd0
aprisyta <aprisyta10@gmail.com>
2018-10-02 03:37:35 +0530

    New Deck TabView created

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,9 @@ class MyApp extends StatelessWidget {
                       itemBuilder: (BuildContext context,int index){
                         return new Container(
                           child: ListTile(
+                            onTap: () {
+                              // open page
+                            },
                             title: Center(child: Text("${decks[index].deckTitle}")),
                             subtitle: Center(child: Text("${decks[index].cards.length} cards")),
                           ),
@@ -41,7 +44,60 @@ class MyApp extends StatelessWidget {
                         );
                       }
                     ),
-                  new Text("Hi")
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: new Column(
+                      children: <Widget>[
+                        Text(
+                          "What is the title of your deck?",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 60.0,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(15.0, 50.0, 15.0, 50.0),
+                          child: TextField(
+                            textAlign: TextAlign.center,
+                            autocorrect: true,
+                            style: TextStyle(
+                              fontSize: 20.0
+                            ),
+                            decoration: InputDecoration(
+                              hintText: "Deck Title",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                                borderSide: BorderSide(
+                                  color: Colors.black,
+                                  width: 2.0,
+                                  style: BorderStyle.solid
+                                )
+                              )
+                            ),
+                          ),
+                        ),
+                        RaisedButton(
+                          child: Text(
+                            "Submit",
+                            style: TextStyle(
+                              fontSize: 20.0,
+                            ),
+                          ),
+                          color: Colors.black,
+                          onPressed: () {
+                            // add deck to decks
+                          },
+                          textColor: Colors.white,
+                          padding: EdgeInsets.fromLTRB(40.0, 15.0, 40.0, 15.0),
+                          shape: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
                 ]
             ),
           )
